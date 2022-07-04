c93f7d905fabe7bbe89c2b370a36f94550fddb92
Wellington Leite <wellingtonleitee.s@gmail.com>
2022-07-04 00:31:07 -0300

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>💖 Hello World!</h1>
+        <p>Welcome to your Electron application.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
